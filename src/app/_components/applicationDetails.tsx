fd4c26bd41eaa9dc87ae1502e55229158293f10b
SureshKamediya <sureshkamediya18@gmail.com>
2025-11-20 00:57:56 +0530

    Minor change to back button on applicaiton detail

--- src/app/_components/applicationDetails.tsx
+++ src/app/_components/applicationDetails.tsx
@@ -224,7 +224,7 @@ export function ApplicationDetails({
           {downloadPdf.isPending ? "Downloading..." : "Download Application PDF"}
         </button>
         <Link
-          href="/application-lookup"
+          href="/"
           className="flex-1 px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700 text-center font-medium"
         >
           Back
